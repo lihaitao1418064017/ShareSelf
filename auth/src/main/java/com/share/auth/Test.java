7b10706fa5ccfd9ec478c7aea3db07d8c3a1aac5
lihaitao <984695425@qq.com>
2018-11-27 21:31:04 +0800

    提交各个模块

---
+++ auth/src/main/java/com/share/auth/Test.java
@@ -0,0 +1,10 @@
+package com.share.auth;
+
+/**
+ * Description:
+ *
+ * @Author lht
+ * @Date 2018/11/27 下午9:30
+ **/
+public class Test {
+}
---
+++ core/src/main/java/com/share/core/Test.java
@@ -0,0 +1,10 @@
+package com.share.core;
+
+/**
+ * Description:
+ *
+ * @Author lht
+ * @Date 2018/11/27 下午9:30
+ **/
+public class Test {
+}
